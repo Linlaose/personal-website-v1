a8f4b443162e48c78200f4d2b3dd4c717dc79e4d
Ryan <aa0900144903@gmail.com>
2025-04-14 22:01:12 +0800

    chore: init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="mx-auto min-h-screen max-w-[1440px] gap-16 font-[family-name:var(--font-geist-sans)] xl:px-32">
+      <section>
+        <header>
+          <h1>Ryan Tsai</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+      </section>
+    </main>
+  );
+}

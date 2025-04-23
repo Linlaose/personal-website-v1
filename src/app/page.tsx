e642572863663f21a13e58262b0bc70f3d259c30
Ryan <aa0900144903@gmail.com>
2025-04-23 20:34:39 +0800

    feat: add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { NAV_LINKS } from "./consts";
 export default function Home() {
   return (
     <main className="min-h-screen font-[family-name:var(--font-inter)]">
-      <header className="bg-primary">
+      <header className="bg-navbar">
         <div className="container flex items-center justify-between xl:py-6">
           <h1 className="heading-gradient-flow text-text-primary px-4 text-2xl leading-tight font-bold">
             Ryan
@@ -26,28 +26,22 @@ export default function Home() {
           </nav>
         </div>
       </header>
-      <section id="banner">
-        <div className="container">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-center font-[family-name:var(--font-roboto-mono)] text-sm/6 sm:text-left">
-            <li className="mb-2 tracking-[-.01em]">
-              Get started by editing{" "}
-              <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-roboto-mono)] font-semibold dark:bg-white/[.06]">
-                src/app/page.tsx
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Save and see your changes instantly.
-            </li>
-          </ol>
+      <section id="hero" className="bg-hero">
+        <div className="container xl:py-40">
+          <div>
+            <h2 className="heading-gradient w-fit text-7xl leading-120 font-bold uppercase">
+              frontend
+              <br />
+              developer
+            </h2>
+            <h4 className="mt-2 text-lg leading-120">
+              I am a web developer with a passion for creating beautiful and
+              responsive websites.
+            </h4>
+          </div>
+          <button type="button" className="button px-4 py-3">
+            view my work
+          </button>
         </div>
       </section>
       <section id="services">

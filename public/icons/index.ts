76631f9f6b810cfd29109b9430a9ed466d5cf949
Ryan <aa0900144903@gmail.com>
2025-06-02 00:08:26 +0800

    chore: add icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -2,5 +2,20 @@ import CSSIcon from "./CSS-icon.svg";
 import HTMLIcon from "./HTML-icon.svg";
 import ReactIcon from "./React-icon.svg";
 import TypeScriptIcon from "./Typescript-icon.svg";
+import GitIcon from "./Git-icon.png";
+import MuiIcon from "./MUI-icon.svg";
+import TailwindIcon from "./Tailwind-icon.png";
+import DocusaurusOpenApiIcon from "./Docusaurus-openAPI-docs-icon.png";
+import NextIcon from "./Next-icon.svg";
 
-export { CSSIcon, HTMLIcon, ReactIcon, TypeScriptIcon };
+export {
+  CSSIcon,
+  HTMLIcon,
+  ReactIcon,
+  TypeScriptIcon,
+  GitIcon,
+  MuiIcon,
+  TailwindIcon,
+  DocusaurusOpenApiIcon,
+  NextIcon,
+};

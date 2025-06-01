25042fe43ead92640ee764491f732ed366ffd92c
Ryan <aa0900144903@gmail.com>
2025-06-01 21:10:12 +0800

    feat: add a index file to export icons

---
+++ public/icons/index.ts
@@ -0,0 +1,6 @@
+import CSSIcon from "./CSS-icon.svg";
+import HTMLIcon from "./HTML-icon.svg";
+import ReactIcon from "./React-icon.svg";
+import TypeScriptIcon from "./Typescript-icon.svg";
+
+export { CSSIcon, HTMLIcon, ReactIcon, TypeScriptIcon };
